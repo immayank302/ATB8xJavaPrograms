0368d3d1b9e6ef49ed1709d442866e7b2591d2e8
MAYANK SINGH <4G53v#@mge>
2024-09-16 09:11:20 +0530

    New prgm added with the new statements  in the Lab002

--- src/sept/ex_04092024/Lab002.java
+++ src/sept/ex_04092024/Lab002.java
@@ -3,5 +3,7 @@ package sept.ex_04092024;
 public class Lab002 {
     public static void main(String[] args) {
           System.out.println("This is a java program");
+          System.out.println("We are learning Java Program");
+          System.out.println("This is awesome");
     }
 }
